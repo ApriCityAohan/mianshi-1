4ce11f5381c97b9d7cbf6d32c7943c40de979d8a
ApriCityAohan <1448366829@qq.com>
2023-07-09 13:29:19 +0800

    update:更新跳转

--- mianshi/src/page/index.vue
+++ mianshi/src/page/index.vue
@@ -1,5 +1,6 @@
 <template>
 	<div>Login</div>
+	<router-link to="/search">跳转搜索页面</router-link>
 </template>
 
 <script setup></script>
--- mianshi2/src/page/search/Search.jsx
+++ mianshi2/src/page/search/Search.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 function Search() {
 	return (
 		<div>
 			<h1>Search</h1>
+			<Link to="/login">登入页跳转</Link>
 		</div>
 	);
 }
